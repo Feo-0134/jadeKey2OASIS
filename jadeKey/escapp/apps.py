dad60d06b4f901b6967f59f0571cb245e05fe307
Feo-0134 <zhujuncheng1021@gmail.com>
2019-12-19 10:46:30 +0800

    remove useless apps

--- jadeKey/escapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EscappConfig(AppConfig):
-    name = 'escapp'
--- jadeKey/jadeK/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Engineer)
-admin.site.register(Process)
-admin.site.register(Stage)
-admin.site.register(Comment)
-
