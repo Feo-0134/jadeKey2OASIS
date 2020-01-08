c55e6eabc2d4b522554cc05d9b217c63cdc99a65
Feo-0134 <zhujuncheng1021@gmail.com>
2020-01-08 20:24:10 +0800

    add model & serializer

--- drf_server/escBackend/models.py
+++ drf_server/escBackend/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class EngineerTitle(models.Model):
+    FullName = models.CharField(maxlength=20)
+    Abbreviation = models.CharField(maxlength=10)
+    def __str__(self):
+        return str(self.id) + ': ' + self.FullName
+
+class ProcessKind(models.Model):
+    FullName = models.CharField(maxlength=20)
+    Abbreviation = models.CharField(maxlength=10)
+    def __str__(self):
+        return str(self.id) + ': ' + self.FullName
+
+class StageKind(models.Model):
+    FullName = models.CharField(maxlength=20)
+    Abbreviation = models.CharField(maxlength=10)
+    def __str__(self):
+        return str(self.id) + ': ' + self.FullName
+
+class Engineer(models.Model):
+    Alias = models.CharField(maxlength=10)
+    Name = models.CharField(maxlength=20)
+    Title = models.ForeignKey(EngineerTitle, ondelete=models.CASCADE)
+    EngineerLatestProcess = models.BigIntegerField(default=-1)
+    EngineerLatestStage = models.BigIntegerField(default=-1)
+    IsReviewer = models.BooleanField(default=False)
+    def __str__(self):
+        return str(self.id) + ': ' + self.Name
+
+class Process(models.Model):
+    Kind = models.ForeignKey(ProcessKind, ondelete=models.CASCADE)
+    ProcessOwner = models.ForeignKey(Engineer, ondelete=CASCADE)
+    ProcessReviewer = models.ManyToManyField(
+        Engineer,
+        through = 'ProcessReview',
+        throughfields = ('Process', 'Reviewer')
+    )
+    ProcessCurrentStage = models.ForeignKey(StageKind, ondelete=CASCADE)
+    Stage1TryTimes = models.BigIntegerField(default=-1)
+    Stage2TryTimes = models.BigIntegerField(default=-1)
+    Stage3TryTimes = models.BigIntegerField(default=-1)
+    Stage4TryTimes = models.BigIntegerField(default=-1)
+    def __str__(self):
+        return str(self.id) + ': ' + self.Owner.Name + ': ' + self.Kind.FullName
+
+class ProcessReview(models.Model):
+    Process = models.ForeignKey(Process, ondelete=models.CASCADE)
+    Reviewer = models.ForeignKey(Engineer, ondelete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ': ' + self.Process.Owner.Name + ': ' + self.Process.Kind.FullName + ': ' + self.Reviewer.Name
+
+class Comment(models.Model):
+    Process = models.ForeignKey(Process, ondelete=models.CASCADE)
+    Stage = models.ForeignKey(StageKind, ondelete=models.CASCADE)
+    Writer = models.ForeignKey(Engineer, ondelete=models.CASCADE)
+    Edited = models.BooleanField(default=False)
+    Submited = models.BooleanField(default=False)
+    def __str__(self):
+        return str(self.id) + ': ' + self.Process.Owner.Name
+         + ': ' + self.Process.Kind.FullName + ': ' + self.Stage.FullName
+         + ': ' + self.Writer.Name 
---
+++ drf_server/escBackend/serializers.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import *
+from rest_framework import serializers
+from .models import *
+
+class EngineerTitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EngineerTitle
+        fields = ['id', 'FullName', 'Abbreviation']
+    
+class ProcessKindSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProcessKind
+        fields = ['id', 'FullName', 'Abbreviation']
+    
+class StageKindSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StageKind
+        fields = ['id', 'FullName', 'Abbreviation']
+
+class EngineerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Engineer
+        fields = ['id', 'Alias', 'Name', 'Title', 'EngineerLatestProcess', 'EngineerLatestStage', 'IsReviewer']
+
+class ProcessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Process
+        fields = ['id', 'Kind', 'ProcessOwner', 'ProcessReviewer', 'ProcessCurrentStage', 'Stage1TryTimes', 'Stage2TryTimes', 'Stage3TryTimes', 'Stage4TryTimes']
+
+class ProcessReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProcessReview
+        fields = ['id', 'Process', 'Reviewer']
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['Process', 'Stage', 'Writer', 'Edited', 'Submited']
