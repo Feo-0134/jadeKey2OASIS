aba81a741ed7c55e7fbdc8712844477ee111ea31
Feo-0134 <zhujuncheng1021@gmail.com>
2020-01-09 11:04:45 +0800

    update view.py and create urls.py

---
+++ drf_server/escBackend/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from escBackend import views
+from rest_framework import renderers
--- drf_server/escBackend/views.py
+++ drf_server/escBackend/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class EngineerTitleViewSet(viewsets.ModelViewSet):
+    queryset = EngineerTitle.objects.all()
+    serializer_class = EngineerTitleSerializer
+
+class ProcessKindViewSet(viewsets.ModelViewSet):
+    queryset = ProcessKind.objects.all()
+    serializer_class = ProcessKindSerializer
+
+class StageKindViewSet(viewsets.ModelViewSet):
+    queryset = StageKind.objects.all()
+    serializer_class = StageKindSerializer
+
+class EngineerViewSet(viewsets.ModelViewSet):
+    queryset = Engineer.objects.all()
+    serializer_class = EngineerSerializer
+
+class ProcessViewSet(viewsets.ModelViewSet):
+    queryset = Process.object.all()
+    serializer_class = ProcessSerializer
+
+class ProcessReviewViewSet(viewsets.ModelViewSet):
+    queryset = ProcessReview.object.all()
+    serializer_class = ProcessReviewSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
