c99977dd12ef6862810d377c9fc727caa8e32362
Feo-0134 <zhujuncheng1021@gmail.com>
2020-01-13 13:45:58 +0800

    init default input and output form for vue pages

---
+++ demo_client/src/components/DefaultInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container>
+        {{}}
+    </v-container>
+</template>
+<script>
+export default {
+    props: {
+
+    },
+    components: {},
+    data: () => ({
+        engineer_id: null,
+    }),
+    asyncComputed: {
+        dada: {
+            async get() {
+                try {
+                    const res = await this.$http.get(`http://127.0.0.1:8000/escBackend/engineer/${this.engineer_id}`)
+                    return res.data
+                }catch(e) {
+                    window.console.log(e)
+                }
+            }
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+
+}
+</script>
--- demo_client/src/main.js
+++ demo_client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+// import Axios from 'axios'
 
 Vue.config.productionTip = false
 
