24b2f45b1237e81bbf3c7c969809c832b260cdca
Feo-0134 <zhujuncheng1021@gmail.com>
2020-01-15 10:44:51 +0800

    update process color

--- demo_client/src/components/Process.vue
+++ demo_client/src/components/Process.vue
@@ -93,7 +93,7 @@
                 </v-col>
                 <v-col cols="12">
                 <v-card
-                    color="#385F73"
+                    color="#5F6062"
                     dark
                 >
                     <v-card-title class="headline">Review Complete</v-card-title>
@@ -104,48 +104,7 @@
             </v-row>
             </v-container>
         </v-card>
-        <v-timeline
-        :align-top="alignTop"
-        :dense="dense"
-        :reverse="reverse">
-        <v-timeline-item
-            v-for="n in 3"
-            :key="n"
-            :fill-dot="fillDot"
-            :hide-dot="hideDot"
-            :icon="icon ? 'mdi-star' : ''"
-            :icon-color=" iconColor ? 'deep-orange' : ''"
-            :left="left"
-            :right="right"
-            :small="small"
-        >
-            <template v-slot:icon>
-            <v-avatar v-if="avatar">
-                <img src="http://i.pravatar.cc/64">
-            </v-avatar>
-            </template>
-            <span slot="opposite">Tus eu perfecto</span>
-            <v-card class="elevation-2">
-            <v-card-title class="headline">Lorem ipsum</v-card-title>
-            <v-card-text>
-                Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-            </v-card-text>
-            </v-card>
-        </v-timeline-item>
-        </v-timeline>
-        <!-- <v-radio-group row wrap>
-        <v-switch v-model="alignTop" label="Toggle align-top" class="mx-4"></v-switch>
-        <v-switch v-model="dense" label="Toggle dense" class="mx-4"></v-switch>
-        <v-switch v-model="fillDot" label="Toggle fill-dot" class="mx-4"></v-switch>
-        <v-switch v-model="hideDot" label="Toggle hide-dot" class="mx-4"></v-switch>
-        <v-switch v-model="icon" label="Toggle icon" class="mx-4"></v-switch>
-        <v-switch v-model="avatar" label="Toggle avatar" class="mx-4"></v-switch>
-        <v-switch v-model="iconColor" label="Toggle icon color" class="mx-4"></v-switch>
-        <v-switch v-model="reverse" label="Toggle reverse" class="mx-4"></v-switch>
-        <v-switch v-model="left" label="Toggle left" class="mx-4"></v-switch>
-        <v-switch v-model="right" label="Toggle right" class="mx-4"></v-switch>
-        <v-switch v-model="small" label="Toggle small" class="mx-4"></v-switch>
-        </v-radio-group> -->
+        
     </v-container>
 </template>
 <script>
@@ -156,38 +115,28 @@ export default {
     components: {},
     data: () => ({
         stage_cnt: 0,
-        alignTop: false,
-        avatar: false,
-        dense: true,
-        fillDot: false,
-        hideDot: false,
-        icon: false,
-        iconColor: false,
-        left: false,
-        reverse: false,
-        right: false,
-        small: false,
+        
         items: [
             {
-            color: '#1F7087',
+            color: '#5F6062',
             src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
             title: 'Submit Request',
             artist: 'Foster the People',
             },
             {
-            color: '#952175',
+            color: '#8E8989',
             src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
             title: 'Checklist Review',
             artist: 'Ellie Goulding',
             },
             {
-            color: '#1F7087',
+            color: '#5F6062',
             src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
             title: 'Submit Request',
             artist: 'Foster the People',
             },
             {
-            color: '#952175',
+            color: '#8E8989',
             src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
             title: 'Checklist Review',
             artist: 'Ellie Goulding',
